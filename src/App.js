5ef1dd3de019dad21ad0b4520326f48454b004e0
gaboliveiradev <g38789138@gmail.com>
2023-10-20 12:11:56 -0300

    feat: router login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import './App.css';
+import Routing from "Routes";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Routing />
   );
 }
 
